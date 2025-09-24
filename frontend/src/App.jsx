526aced230f99206115cf6d763130bb5c9f71511
Itxz-divyu07 <divya.ya777@gmail.com>
2025-09-24 12:16:58 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import './App.css'
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/home/Home.jsx";
 import Service from "./Pages/services/service.jsx";
