ec9b8a8ed6717b748c268edc2a6e0cdcee04045c
allwinbarath3081994-eng <allwinbarath3081994@gmail.com>
2025-10-14 10:50:15 +0530

    Save local changes before merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { FiltersProvider } from "./Components/Context/FiltersContext.jsx";
+import "./i18n/i18n.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
